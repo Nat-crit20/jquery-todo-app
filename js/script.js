5980dd14afcec60d21af0d3a94ef48ac71ee2583
Nat-crit20 <93047867+Nat-crit20@users.noreply.github.com>
2023-01-24 15:08:50 -0600

    Updated crossOutButton to jquery

--- js/script.js
+++ js/script.js
@@ -20,14 +20,14 @@ function newItem() {
   li.on("dblclick", crossOut);
 
   //3(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
 
-  crossOutButton.addEventListener("click", deleteListItem);
+  crossOutButton.on("click", deleteListItem);
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem() {
-    li.classList.add("delete");
+    li.addClass("delete");
   }
   // 4. Reordering the items:
   $("#list").sortable();
